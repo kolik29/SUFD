43e2b03bdfebface60abe4c202dab01ffe9cbddb
kolik29 <30481034+kolik29@users.noreply.github.com>
2018-05-22 10:36:54 +0300

    1

---
+++ SUFD/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace SUFD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.num = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.date1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.date2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.summ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.num_p = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kbk = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nazn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.okato = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FIO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Size = new System.Drawing.Size(1101, 583);
+            this.splitContainer1.SplitterDistance = 540;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.num,
+            this.date1,
+            this.date2,
+            this.summ,
+            this.num_p,
+            this.kbk,
+            this.nazn,
+            this.okato,
+            this.FIO});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.MinimumSize = new System.Drawing.Size(0, 396);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1101, 540);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // num
+            // 
+            this.num.HeaderText = "№";
+            this.num.Name = "num";
+            this.num.Width = 43;
+            // 
+            // date1
+            // 
+            this.date1.HeaderText = "Дата платежа";
+            this.date1.Name = "date1";
+            this.date1.Width = 96;
+            // 
+            // date2
+            // 
+            this.date2.HeaderText = "Дата выписки";
+            this.date2.Name = "date2";
+            this.date2.Width = 96;
+            // 
+            // summ
+            // 
+            this.summ.HeaderText = "Сумма";
+            this.summ.Name = "summ";
+            this.summ.Width = 66;
+            // 
+            // num_p
+            // 
+            this.num_p.HeaderText = "Номер платежки";
+            this.num_p.Name = "num_p";
+            this.num_p.Width = 108;
+            // 
+            // kbk
+            // 
+            this.kbk.HeaderText = "КБК";
+            this.kbk.Name = "kbk";
+            this.kbk.Width = 53;
+            // 
+            // nazn
+            // 
+            this.nazn.HeaderText = "Назначение платежа";
+            this.nazn.Name = "nazn";
+            this.nazn.Width = 127;
+            // 
+            // okato
+            // 
+            this.okato.HeaderText = "ОКАТО";
+            this.okato.Name = "okato";
+            this.okato.Width = 69;
+            // 
+            // FIO
+            // 
+            this.FIO.HeaderText = "ФИО";
+            this.FIO.Name = "FIO";
+            this.FIO.Width = 59;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 583);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "SUFD";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn num;
+        private System.Windows.Forms.DataGridViewTextBoxColumn date1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn date2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn summ;
+        private System.Windows.Forms.DataGridViewTextBoxColumn num_p;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kbk;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nazn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn okato;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FIO;
+    }
+}
+
---
+++ SUFD/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Collections.Generic;
+
+namespace SUFD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int numRow = 0;
+        List<XElement> elements = new List<XElement>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            XDocument xdoc = new XDocument();
+
+            ofd.Filter = "XML файлы (*.xml)|*.xml|Все файлы (*.*)|*.*";
+            ofd.FilterIndex = 1;
+            ofd.RestoreDirectory = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((ofd.OpenFile()) != null)
+                    {
+                        xdoc = XDocument.Load(ofd.FileName);
+
+                        string num_pp = "", kbk = "", okato = "";
+
+                        foreach (XElement node in xdoc.Descendants())
+                            if (node.Name.ToString() == "NOM_LINE")
+                                dataGridView1.Rows.Add();
+
+                        foreach (XElement node in xdoc.Descendants())
+                        {
+                            if (node.Name.ToString() == "NOM_LINE")
+                            {
+                                dataGridView1.Rows[numRow].Cells[0].Value = node.Value;
+                                elements.Add(new XElement(node.Name.ToString(), node.Value));
+                            }
+
+                            if (node.Name.ToString() == "DATE_PAY_DOC")
+                            {
+                                dataGridView1.Rows[numRow].Cells[1].Value = node.Value;
+                                elements.Add(new XElement(node.Name.ToString(), node.Value));
+                            }
+
+                            if (node.Name.ToString() == "DATE_REESTR_PP")
+                            {
+                                dataGridView1.Rows[numRow].Cells[2].Value = node.Value;
+                                elements.Add(new XElement(node.Name.ToString(), node.Value));
+                            }
+
+                            if (node.Name.ToString() == "SUM_REESTR_PP")
+                            {
+                                dataGridView1.Rows[numRow].Cells[3].Value = node.Value.Substring(0, node.Value.Length - 2) + "," + node.Value.Substring(node.Value.Length - 2);
+                                elements.Add(new XElement(node.Name.ToString(), node.Value));
+                            }
+
+                            if (node.Name.ToString() == "NOM_PP")
+                            {
+                                num_pp = node.Value;
+                                elements.Add(new XElement(node.Name.ToString(), node.Value));
+                            }
+
+                            if (numRow != dataGridView1.RowCount - 1)
+                                dataGridView1.Rows[numRow].Cells[4].Value = num_pp;
+
+                            if (node.Name.ToString() == "KBK")
+                            {
+                                kbk = node.Value;
+                                elements.Add(new XElement(node.Name.ToString(), node.Value));
+                            }
+
+                            if (numRow != dataGridView1.RowCount - 1)
+                                dataGridView1.Rows[numRow].Cells[5].Value = kbk;
+
+                            if (node.Name.ToString() == "PURPOSE")
+                            {
+                                dataGridView1.Rows[numRow].Cells[6].Value = node.Value.Split(';')[2].Replace("НАЗНАЧЕНИЕ2:", "");
+                                elements.Add(new XElement(node.Name.ToString(), node.Value));
+                                numRow++;
+                            }
+
+                            if (node.Name.ToString() == "OKATO")
+                            {
+                                okato = node.Value;
+                                elements.Add(new XElement(node.Name.ToString(), node.Value));
+                            }
+
+                            if (numRow != dataGridView1.RowCount - 1)
+                                dataGridView1.Rows[numRow].Cells[7].Value = okato;
+
+                            if (node.Name.ToString() == "FIO_PLAT")
+                            {
+                                dataGridView1.Rows[numRow].Cells[8].Value = node.Value;
+                                elements.Add(new XElement(node.Name.ToString(), node.Value));
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка. Не удалось открыть файл: " + ex.Message);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            XDocument xdoc = new XDocument();
+            XElement root = new XElement("root");
+            root.Add(elements);
+            xdoc.Add(root);
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "XML файлы (*.xml)|*.xml|Все файлы (*.*)|*.*";
+            sfd.FilterIndex = 1;
+            sfd.RestoreDirectory = true;
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                xdoc.Save(sfd.FileName);
+            }
+        }
+    }
+}
